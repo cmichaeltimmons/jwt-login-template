2ab6c195a899e86436c4a7d13c61fc042b4ceab2
cmichaeltimmons <michaeltimmons@presidio.com>
2018-05-16 02:22:53 -0400

    add pg

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ require('dotenv').config()
 var express = require('express');
 var app = express();
 var router = express.Router();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
