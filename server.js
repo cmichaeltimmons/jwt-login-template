a2f35e9fe33207351f0f87be9b4f07500ac61245
cmichaeltimmons <michaeltimmons@presidio.com>
2018-05-16 03:16:28 -0400

    update db test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table');
+    const result = await client.query('SELECT * FROM user');
     res.render('pages/db', result);
     client.release();
   } catch (err) {
